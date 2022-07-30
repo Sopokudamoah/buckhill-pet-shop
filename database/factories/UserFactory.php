43ea346c29689e59611a3e6b62e34ccd1b45982c
Stephen Opoku Damoah <sopoku22@live.com>
2022-07-30 00:43:38 +0000

    Add additional user attributes to UserFactory class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
 class UserFactory extends Factory
 {
@@ -18,11 +17,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'is_admin' => fake()->randomElement(['0', '1']),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => fake()->dateTime(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'address' => fake()->address(),
+            'phone_number' => fake()->e164PhoneNumber(),
+            'is_marketing' => fake()->randomElement(['0', '1']),
         ];
     }
 

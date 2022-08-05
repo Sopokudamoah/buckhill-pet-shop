ee6d6045cce45bace74d61a1df05aa05a6b089eb
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-05 14:33:40 +0000

    Add sample json response files for generating API docs

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,10 +36,16 @@ return [
             'throw' => false,
         ],
 
+        'responses' => [
+            'driver' => 'local',
+            'root' => storage_path('responses'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],

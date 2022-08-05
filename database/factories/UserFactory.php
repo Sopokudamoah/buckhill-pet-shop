67521fe25d867052b376758b7928240893faf7b8
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-05 16:11:40 +0000

    PSR12 formatting

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,13 +19,14 @@ class UserFactory extends Factory
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
-            'is_admin' => fake()->randomElement(['0', '1']),
+            'is_admin' => 0,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => fake()->dateTime(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('userpassword'),
             'address' => fake()->address(),
             'phone_number' => fake()->e164PhoneNumber(),
             'is_marketing' => fake()->randomElement(['0', '1']),
+            'avatar' => fake()->uuid()
         ];
     }
 
@@ -39,6 +40,25 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    public function isAdmin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'is_admin' => 1,
+                'password' => bcrypt('admin')
+            ];
+        });
+    }
+
+    public function plainPassword()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'password' => $attributes['is_admin'] ? 'admin' : 'userpassword'
             ];
         });
     }

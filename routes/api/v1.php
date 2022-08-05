a1f49efe7a87bbf0780e9f7ab99afbd10b5abbd2
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-05 13:14:01 +0000

    Move admin logout route to middleware group

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -7,10 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('admin.')->controller(AdminController::class)->group(function () {
     Route::post('login', 'login')->name('login');
-    Route::get('logout', 'logout')->name('logout');
-
 
     Route::middleware(['auth:sanctum', AdminMiddleware::class])->group(function () {
+        Route::get('logout', 'logout')->name('logout');
         Route::post('create', 'create')->name('create');
         Route::get('user-listing', 'userListing')->name('user-listing');
         Route::put('user-edit/{user}', 'userEdit')->name('user-edit');

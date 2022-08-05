89e853c0557a1a7944d871ed14560542e3b96bb3
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-05 21:51:01 +0000

    Create user resource and request classes

---
+++ app/Http/Requests/V1/UserListingRequest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserListingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'page' => 'sometimes|int',
+            'per_page' => 'sometimes|int',
+            'filter.first_name' => 'string',
+            'filter.last_name' => 'string',
+            'filter.email' => 'string',
+            'filter.phone_number' => 'string',
+            'sorting' => 'sometimes|string',
+        ];
+    }
+
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function queryParameters()
+    {
+        return [
+            'page' => [
+                'description' => 'The page number',
+                'example' => 1
+            ],
+            'per_page' => [
+                'description' => 'Number of resources per page',
+                'example' => 15
+            ],
+            'filter.first_name' => [
+                'description' => 'Filter by the first_name field',
+                'example' => fake()->firstName()
+            ],
+            'filter.last_name' => [
+                'description' => 'Filter by the last_name field',
+                'example' => fake()->lastName()
+            ],
+            'filter.email' => [
+                'description' => 'Filter by the email field',
+                'example' => fake()->safeEmail()
+            ],
+            'filter.phone_number' => [
+                'description' => 'Filter by the phone_number field',
+                'example' => fake()->e164PhoneNumber()
+            ],
+            'sorting' => [
+                'description' => 'Sort by field. You can sort by multiple fields by comma-separating them. A negative(-) prefix denotes a descending sort. ',
+                'example' => 'name,email'
+            ]
+        ];
+    }
+}

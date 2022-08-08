e281fc2f70265b26dbeb9223713a42a373db6584
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-08 19:43:45 +0000

    Add action for product list route

---
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User\V1\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+/**
+ * @group Products endpoint
+ *
+ * This endpoint handles the CRUD methods for the Products.
+ */
+class ProductController extends Controller
+{
+    /**
+     * List all products
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/products-listing-200.json
+     * @responseFile status=400 scenario="when filtered by disallowed field" storage/responses/products-listing-400.json
+     */
+    public function index(Request $request)
+    {
+        $products = QueryBuilder::for(Product::query())
+            ->with(['category'])
+            ->select(['title', 'uuid', 'category_uuid', 'description', 'metadata'])
+            ->allowedFilters(['title', 'uuid', 'category_uuid'])
+            ->simplePaginate($request->get('per_page', 15));
+
+
+        return (new ProductResource())->resource($products);
+    }
+}
---
+++ app/Http/Resources/User/V1/ProductResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Resources\User\V1;
+
+use App\Http\Resources\V1\BaseApiResource;
+
+class ProductResource extends BaseApiResource
+{
+}

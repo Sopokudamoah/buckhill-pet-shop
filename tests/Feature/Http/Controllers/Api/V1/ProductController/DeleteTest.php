2b08fab3b20be4ee6407203f63204b6226613677
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-08 21:36:32 +0000

    Add test for product delete action

---
+++ tests/Feature/Http/Controllers/Api/V1/ProductController/DeleteTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+
+test('user can delete product', function () {
+    $user = User::factory()->create();
+
+    $token = $user->createToken()->plainTextToken;
+
+    $product = Product::factory()->for(Category::factory())->create();
+
+    $this->assertModelExists($product);
+    $response = apiTest()->withToken($token)->delete(route('api.v1.product.delete', $product));
+
+    $response->assertStatus(200);
+    $this->assertNull(Product::find($product->id));
+    #The line below is of generating response samples for API documentation
+//    Storage::drive('responses')->put('delete-product-200.json', $response->content());
+});
+
+test('user cannot delete product if uuid is invalid', function () {
+    $user = User::factory()->create();
+
+    $token = $user->createToken()->plainTextToken;
+
+    $response = apiTest()->withToken($token)->delete(route('api.v1.product.delete', fake()->uuid()));
+
+    $response->assertStatus(404);
+
+    #The line below is of generating response samples for API documentation
+//    Storage::drive('responses')->put('delete-product-404.json', $response->content());
+});

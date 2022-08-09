7d3fd5a2bc1a077203a27a5b9ed00fcbc647a558
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-09 12:02:20 +0000

    Add controller, request and resource classes for category endpoint

---
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\V1\CreateCategoryRequest;
+use App\Http\Requests\Category\V1\UpdateCategoryRequest;
+use App\Http\Resources\V1\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
+/**
+ * @group Categories endpoint
+ *
+ * This endpoint handles the CRUD methods for the Categories.
+ */
+class CategoryController extends Controller
+{
+    /**
+     * List all categories
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/categories-listing-200.json
+     * @responseFile status=400 scenario="when filtered by disallowed field" storage/responses/categories-listing-400.json
+     */
+    public function index(Request $request)
+    {
+        $products = QueryBuilder::for(Category::query())
+            ->select(['title', 'uuid', 'slug'])
+            ->allowedFilters(['title', 'uuid', 'slug'])
+            ->simplePaginate($request->get('per_page', 15));
+
+
+        return (new CategoryResource())->resource($products);
+    }
+
+
+    /**
+     * Create category
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/create-category-200.json
+     * @responseFile status=422 scenario="when validation fails" storage/responses/create-category-422.json
+     */
+    public function create(CreateCategoryRequest $request)
+    {
+        $data = $request->validated();
+        $category = Category::create($data);
+
+        return (new CategoryResource($category))->message("Category created successfully");
+    }
+
+
+    /**
+     * Update category
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/update-category-200.json
+     * @responseFile status=422 scenario="when validation fails" storage/responses/update-category-422.json
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $data = $request->validated();
+
+        $category->fill($data);
+
+        if ($category->isDirty()) {
+            $category->save();
+        }
+
+        return (new CategoryResource($category))->message("Category updated successfully");
+    }
+
+
+    /**
+     * Show category information
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/show-category-200.json
+     * @responseFile status=404 scenario="when uuid is invalid" storage/responses/show-category-404.json
+     */
+    public function show(Category $category)
+    {
+        return (new CategoryResource($category));
+    }
+
+
+    /**
+     * Delete category
+     *
+     * @authenticated
+     *
+     * @responseFile status=200 storage/responses/delete-category-200.json
+     * @responseFile status=404 scenario="when uuid is invalid" storage/responses/delete-category-404.json
+     */
+    public function delete(Category $category)
+    {
+        $category->delete();
+        return (new CategoryResource())->message("Category deleted");
+    }
+}
---
+++ app/Http/Resources/V1/CategoryResource.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+class CategoryResource extends BaseApiResource
+{
+}

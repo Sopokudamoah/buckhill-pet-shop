598177a135a7b3cbcef138aa91aac8b8ff00597c
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-10 15:35:24 +0000

    Add order migration, factory and seeder classes

---
+++ database/migrations/2022_08_10_151925_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\OrderStatus;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->foreignIdFor(OrderStatus::class)->references('id')->on('order_statuses');
+            $table->foreignIdFor(Payment::class)->references('id')->on('payments');
+            $table->uuid()->unique();
+            $table->json('products');
+            $table->json('delivery_fee');
+            $table->float('address')->nullable();
+            $table->float('amount');
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};

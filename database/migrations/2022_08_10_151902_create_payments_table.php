61ab9e9701324a3afcd42b4e74eb52417e727158
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-10 15:32:54 +0000

    Add payment migration, factory and seeder classes

---
+++ database/migrations/2022_08_10_151902_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->string('type', 50)->index();
+            $table->json('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};

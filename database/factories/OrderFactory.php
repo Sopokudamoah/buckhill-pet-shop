7d858b7656a93784cfecf41abadeeb0700656943
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-10 17:04:41 +0000

    Define factory for payment, order and order status models

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
+ * @property float $total
  * @extends Factory<Order>
  */
 class OrderFactory extends Factory
@@ -17,8 +20,20 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+        $products = Product::factory()->for(Category::factory())->count(fake()->numberBetween(1, 5))->create();
+
+        $products = $products->map(function (Product $product) {
+            $quantity = fake()->numberBetween(1, 5);
+            $amount = $product->price * $quantity;
+            $this->total = +$amount;
+            return ['product' => $product->uuid, 'quantity' => $quantity];
+        });
+
         return [
-            //
+            'products' => $products->toArray(),
+            'delivery_fee' => $this->total > 500 ? null : 15,
+            'address' => ['billing' => fake()->streetAddress(), 'shipping' => fake()->streetAddress()],
+            'amount' => $this->total,
         ];
     }
 }
--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Payment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
+ * @extends Factory<Payment>
  */
 class PaymentFactory extends Factory
 {
@@ -16,8 +17,51 @@ class PaymentFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
-        ];
+        return [];
+    }
+
+    public function creditCard()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'credit_card',
+                'details' => [
+                    'holder_name' => fake()->name(),
+                    'number' => fake()->creditCardNumber(),
+                    'ccv' => fake()->numberBetween(111, 999),
+                    'expire_date' => fake()->creditCardExpirationDate()
+                ],
+            ];
+        });
+    }
+
+
+    public function cashOnDelivery()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'cash_on_delivery',
+                'details' => [
+                    'first_name' => fake()->firstName(),
+                    'last_name' => fake()->lastName(),
+                    'address' => fake()->address(),
+                ],
+            ];
+        });
+    }
+
+
+    public function bankTransfer()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'bank_transfer',
+                'details' => [
+                    'swift' => fake()->swiftBicNumber(),
+                    'iban' => fake()->iban(),
+                    'name' => fake()->name(),
+                ],
+            ];
+        });
     }
 }

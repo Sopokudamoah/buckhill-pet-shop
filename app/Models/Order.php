ba6d4af73680c46b25eeda79c559f05bd6fc20e7
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-10 21:37:00 +0000

    Calculate total amount and delivery_fee attributes on update or create

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use App\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use JetBrains\PhpStorm\NoReturn;
 
 class Order extends Model
 {
@@ -25,6 +26,25 @@ class Order extends Model
 
     protected $casts = ['address' => 'array', 'products' => 'array'];
 
+    protected $attributes = ['order_status_id' => 1];
+
+    protected $dates = ['shipped_at'];
+
+    protected static function booted()
+    {
+        self::creating(function (Order $order) {
+            $order->prepareCalculatedValues();
+        });
+
+
+        self::updating(function (Order $order) {
+            $order->prepareCalculatedValues();
+        });
+
+        parent::booted();
+    }
+
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
@@ -38,5 +58,15 @@ class Order extends Model
     public function orderStatus(): BelongsTo
     {
         return $this->belongsTo(OrderStatus::class);
+    }
+
+    #[NoReturn]
+    public function prepareCalculatedValues()
+    {
+        $this->amount = collect($this->products)->sum(function ($product) {
+            return Product::findByUuid($product['product'], ['price'], true)->price * $product['quantity'];
+        });
+
+        $this->delivery_fee = $this->amount > 500 ? null : 15;
     }
 }

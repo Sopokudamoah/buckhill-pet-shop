815d7762a0b8463a4faed10407c92a3331af7cc1
Stephen Opoku Damoah <sopoku22@live.com>
2022-08-11 02:49:30 +0000

    Validate jwt token

--- app/Guards/JwtAuthGuard.php
+++ app/Guards/JwtAuthGuard.php
@@ -4,16 +4,29 @@ namespace App\Guards;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\JoinClause;
 
 /**
  *
  */
 class JwtAuthGuard
 {
+    /**
+     * @var string
+     */
     private string $token;
 
-    public static $user_key = 'user_uuid';
+    /**
+     * @var string
+     */
+    public static string $user_key = 'user_uuid';
 
+    /** @var string */
+    public static string $token_key = 'token_id';
+
+    /**
+     * @param string $token
+     */
     public function __construct(string $token)
     {
         $this->token = $token;
@@ -26,13 +39,22 @@ class JwtAuthGuard
     public function getUserFromToken()
     {
         $payload = jwt_decode($this->token);
-
-        return User::findByUuid($payload[self::$user_key]);
+        return $this->isValidPayload($payload);
     }
 
 
+    /**
+     * @param array $payload
+     * @return User|null
+     */
     private function isValidPayload(array $payload)
     {
-        return isset($payload[self::$user_key]);
+        $user_uuid = $payload[self::$user_key];
+        $token_id = $payload[self::$token_key];
+
+        return User::query()->join('jwt_tokens', function (JoinClause $clause) use ($token_id) {
+            $clause->on('user_id', '=', 'users.id')
+                ->where('jwt_tokens.id', '=', $token_id);
+        })->uuid($user_uuid)->first(['users.*']);
     }
 }
